6250a3d02baa9cc2b4bd78bbb3fd8258654ec922
lindsay cooper <lindsaysaracooper@gmail.com>
2016-06-29 17:39:27 -0500

    homework day 13

---
+++ main.js
@@ -0,0 +1,28 @@
+$( ".hidden_list" ).click(function(){
+$(this).toggleClass("hidden_list");
+
+
+});
+
+//
+// $(".hidden_list").slideDown( "slow", function() {
+//     // Animation complete.
+//   });
+
+//
+// if i click on hidden list, it will remove the hidden list class. if i click on a different class list it will add the hidden class list to the first hidden list and remove the kidden list class from the second
+
+//
+//
+// $(".hidden_list").show("slow");
+//
+//
+//
+// if $(this).toggleClass("hidden_list") {
+//  $(this).slideDown( "slow" );
+//
+// });
+
+//
+// USE THIS IF ANYTHING
+//
